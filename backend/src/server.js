72a439448a6842a24fae590e1e8f3b4e31d9e43e
Rahul Pawar <rp7772633@gmail.com>
2026-03-01 00:14:35 +0530

    Fix CORS for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const { initDatabase } = require('./config/db');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173'],
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
